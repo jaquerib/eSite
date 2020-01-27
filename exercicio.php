e43f0936f3b89e65fa2971ad6c0996df093eed7d
Jaqueline <jaquelineribeiro41@hotmail.com>
2020-01-26 21:20:43 -0300

    novas atualizações

---
+++ exercicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap&subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <link rel="stylesheet" href="recursos/css/exercicio.css">
+    <title>Curso  PHP</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+              
+        </div>
+    </main>
+    <footer class="rodape"> 
+        COD3R & ALUNOS <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
